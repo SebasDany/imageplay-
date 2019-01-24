f33ecdd7e6df6eaa110aab698315221aff624329
SebasDany <sguandinangod@est.ups.edu.ec>
2019-01-24 09:32:54 -0500

    cambio de pantalla

--- Interface.py
+++ Interface.py
@@ -17,20 +17,23 @@ ventana.geometry("800x500")
 ventana.configure(background = colorFondo)
 etiquetaTitulo= Label(ventana, text="Bienvenido",
                       bg="red", fg=colorFondo,width=60).place(x=150,y=10)
-
-
-
-
 botoGuardar = Button(ventana, text="SELECCIONANR UNA IMAGEN", bg="#009",width=35, height=1,
                      fg="White" ).place(x=450, y=100)
-botoConsultar = Button(ventana, text="AÑADIR DESCRIPCION  IMAGEN", bg="#009",width=35, height=1,
-                       fg="white").place(x=450, y=150)
+
+
+etiquetaT = Label(ventana, text="NOMBRE DE LA IMAGEN", bg=colorFondo,
+                  fg=colorLetra,width=35, height=1).place(x=450, y=150)
+cajaT = Entry(ventana, textvariable=titulo, width=40).place(x=450, y=175)
+
 
 botoConsultar = Button(ventana, text="AÑADIR NOMBRE DE LA  IMAGEN", bg="#009",width=35, height=1,
                        fg="white").place(x=450, y=200)
 etiquetaT = Label(ventana, text="DESCRIPCION DE LA IMAGEN", bg=colorFondo,
                   fg=colorLetra,width=35, height=1).place(x=450, y=250)
-cajaT = Entry(ventana, textvariable=titulo, width=40).place(x=450, y=300)
+cajaT = Entry(ventana, textvariable=titulo, width=40).place(x=450, y=275)
+
+botoConsultar = Button(ventana, text="AÑADIR DESCRIPCION  IMAGEN", bg="#009",width=35, height=1,
+                       fg="white").place(x=450, y=300)
 
 botoConsultar = Button(ventana, text="INICIAR", bg="#009",width=35, height=1,
                        fg="white").place(x=450, y=350)
