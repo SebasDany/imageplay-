bf477ca51691f729a63b148a55c7d29d640a33fb
SebasDany <sguandinangod@est.ups.edu.ec>
2019-01-26 11:35:50 -0500

    cambio de pantalla

--- Interface.py
+++ Interface.py
@@ -1,6 +1,20 @@
 from tkinter import *
+from tkinter import filedialog
+
+from tkinter.filedialog import askopenfilename
 from tkinter import messagebox
 from tkinter import ttk
+import image
+
+a=""
+def chose():
+    Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
+    filename = askopenfilename( )  # show an "Open" dialog box and return the path to the selected file
+    print(filename)
+    a=filename
+
+
+
 
 ventana = Tk()
 
@@ -13,34 +27,33 @@ conteliminar = StringVar()
 colorFondo = "BLACK"
 colorLetra = "WHITE"
 ventana.title("Imge Play")
-ventana.geometry("800x500")
-ventana.configure(background = colorFondo)
-etiquetaTitulo= Label(ventana, text="Bienvenido",
-                      bg="red", fg=colorFondo,width=60).place(x=150,y=10)
-botoGuardar = Button(ventana, text="SELECCIONANR UNA IMAGEN", bg="#009",width=35, height=1,
-                     fg="White" ).place(x=450, y=100)
-
+ventana.geometry("800x500+0+0")
+ventana.configure(background=colorFondo)
+etiquetaTitulo = Label(ventana, text="Bienvenido",
+                       bg="red", fg=colorFondo, width=60).place(x=150, y=10)
+botoGuardar = Button(ventana, text="SELECCIONANR UNA IMAGEN", command=chose(), bg="#009", width=35, height=1,
+                     fg="White").place(x=450, y=100)
 
 etiquetaT = Label(ventana, text="NOMBRE DE LA IMAGEN", bg=colorFondo,
-                  fg=colorLetra,width=35, height=1).place(x=450, y=150)
+                  fg=colorLetra, width=35, height=1).place(x=450, y=150)
 cajaT = Entry(ventana, textvariable=titulo, width=40).place(x=450, y=175)
 
-
-botoConsultar = Button(ventana, text="AÑADIR NOMBRE DE LA  IMAGEN", bg="#009",width=35, height=1,
+botoConsultar = Button(ventana, text="AÑADIR NOMBRE DE LA  IMAGEN", bg="#009", width=35, height=1,
                        fg="white").place(x=450, y=200)
 etiquetaT = Label(ventana, text="DESCRIPCION DE LA IMAGEN", bg=colorFondo,
-                  fg=colorLetra,width=35, height=1).place(x=450, y=250)
+                  fg=colorLetra, width=35, height=1).place(x=450, y=250)
 cajaT = Entry(ventana, textvariable=titulo, width=40).place(x=450, y=275)
 
-botoConsultar = Button(ventana, text="AÑADIR DESCRIPCION  IMAGEN", bg="#009",width=35, height=1,
+botoConsultar = Button(ventana, text="AÑADIR DESCRIPCION  IMAGEN", bg="#009", width=35, height=1,
                        fg="white").place(x=450, y=300)
 
-botoConsultar = Button(ventana, text="INICIAR", bg="#009",width=35, height=1,
+botoConsultar = Button(ventana, text="INICIAR", bg="#009", width=35, height=1,
                        fg="white").place(x=450, y=350)
-botoConsultar = Button(ventana, text="SALIR", bg="#009",width=35, height=1,
+botoConsultar = Button(ventana, text="SALIR", bg="#009", width=35, height=1,
                        fg="white").place(x=450, y=400)
 
-r = Text(ventana, width=50 )
-r.insert(INSERT, "IMAGE")
-r.place(x=20, y=60)
+im = PhotoImage(file="C:/Users/USER/OneDrive/Documentos/Reduce-Ubuntu-Load-Time.png")
+
+fond = Label(ventana, image=im, width=400, height="400").place(x=20, y=60)
+
 mainloop()
