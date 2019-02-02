33b336fac9e94ea1e8b87712ab26eea8068e8c27
SebasDany <sguandinangod@est.ups.edu.ec>
2019-02-02 16:31:14 -0500

    Merge remote-tracking branch 'origin/master'
    implementaciondel tiempo y registro

--- Interface.py
+++ Interface.py
@@ -1,6 +1,9 @@
 from distutils import command
 from tkinter import *
 import Validacion
+import datetime, time
+import  datos
+
 import os
 import shutil
 from tkinter import messagebox
@@ -8,6 +11,29 @@ from tkinter import ttk
 from tkinter.filedialog import askopenfilename
 objeto_validacion=Validacion.validacion()
 #SELECCION DE IMAGEN
+
+
+def iniciar(contador=2):
+    jugador = cajaju.get('1.0', END)
+    imnom = cajanombre.get('1.0', END)
+    datos.crerar_archivo(jugador,"4",imnom)
+
+    proceso = 2
+    time['text'] = contador
+    proceso = time.after(1000, iniciar, (contador - 1))
+    if (contador == 0):
+        time.after_cancel(proceso)
+        v = Tk()
+
+        v.title("Image Play")
+        v.configure(background="black")
+        v.geometry("300x100")
+        lbl = Label(v, text=" ** You Lost ** ",font=("", "30"),fg=colorFondo,background="black")
+        lbl.grid(column=0, row=0)
+        v.mainloop()
+
+
+
 def chose():
     Tk().withdraw()  # we don't want a full GUI, so keep the root window from appearing
     filename = askopenfilename()  # show an "Open" dialog box and return the path to the selected file
@@ -63,10 +89,6 @@ def anadir_descripcion():
     objeto_validacion.set_descripcion_imagen(descripcion_imagen)
 #INTERFAZ
 ventana = Tk()
-#############Objeto validacion
-
-
-
 
 genero = StringVar()
 titulo = StringVar()
@@ -81,7 +103,7 @@ ventana.title("Image Play")
 ventana.geometry("800x698")
 ventana.configure(background = colorFondo)
 etiquetaTitulo= Label(ventana, text="BIENVENIDO",
-                      bg="teal", fg=colorFondo,width=60).place(x=190,y=10)
+                      bg="teal", fg=colorFondo,width=40,font=("", "18")).place(x=30,y=10)
 
 etiquetajug = Label(ventana, text="NOMBRE DEL JUGADOR", bg=colorFondo,
                   fg=colorLetra,width=35, height=1).place(x=450, y=50)
@@ -153,18 +175,12 @@ etiquetaT4 = Label(ventana, text="ADIVINA: ", bg=colorFondo,
 
 
 
-
-
-
-
-
-
 #ADIVINA LA PALABRA
 cajajugar =  Text(ventana, height=5, width=38, borderwidth=2).place(x=450, y=550 )
 botoFinaliza = Button(ventana, text="FINALIZAR", bg=colorBotones,width=20, height=1,
                        fg=colorLetra).place(x=610, y=665)
 botoIntentar = Button(ventana, text="INICIAR", bg=colorBotones,width=20, height=1,
-                       fg=colorLetra,command=anadir_nombre).place(x=450, y=665)
+                       fg=colorLetra,command=iniciar).place(x=450, y=665)
 
 
 #REEMPLAZO DE IMAGEN
@@ -173,6 +189,10 @@ label_principal.place(x=20,y=60)
 label_principal.pack
 imh = PhotoImage(file="descarga.png")
 
+cron = Label(ventana, text="Time:",
+                 fg=colorFondo,font=("", "18")).place(x=620, y=10)
+time = Label(ventana, fg='red', width=5, font=("", "18"))
+time.place(x=700, y=10)
 
 center(ventana)
 mainloop()
--- datos.py
+++ datos.py
@@ -1,4 +1,6 @@
 import datetime, time
+from tkinter import *
+
 ##SEBASTIAN GUANDINANGO
 
 
@@ -7,40 +9,27 @@ def crerar_archivo(nombre,tiempo,nomimag):
     print('Mi mimbre:',nombre)
     print('Su tipo es:', tiempo)
     fout = open("registros.txt", "w")
-    fout.write('JUGADOR : '+ nombre+'\n')
-
+    fout.write('JUGADOR : '+nombre+'\n')
     fout.write('TIEMPO : ' + tiempo + '\n')
     fout.write('NOMBRE DE LA IMAGEN : ' + nomimag + '\n')
+
     fout.close()
-def cromometro():
-    suma = 0
-    while (suma <= 10):
-        print("segundos:", suma)
-        suma = suma + 1
-        time.sleep(1)
 
-
-def iniciar(contador=0):
-    global proceso
-
-    # mostramos la variable contandor
+def iniciar(contador=2):
+    proceso = 2
     time['text'] = contador
-
-    # hacemos un llamamient a la funcion mostrarContador pasando el
-    # contador mas uno
-    proceso = time.after(1000, iniciar, (contador + 1))
-
-
-def parar():
-    global proceso
-    time.after_cancel(proceso)
-
-def iniciar(contador=0):
-    proceso = 0
-    time['text'] = contador
-    proceso = time.after(1000, iniciar, (contador + 1))
-    if (contador == 10):
+    proceso = time.after(1000, iniciar, (contador - 1))
+    if (contador == 0):
         time.after_cancel(proceso)
+        v = Tk()
+
+        v.title("Image Play")
+        v.configure(background="black")
+        v.geometry("300x100")
+        lbl = Label(v, text=" ** You Lost ** ",font=("", "30"),background="black")
+        lbl.grid(column=0, row=0)
+        v.mainloop()
+
 
 
 
